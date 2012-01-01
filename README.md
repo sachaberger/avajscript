5908c32ddf474c4f29b96a9095cc827a37e8935f
Sacha BERGER <sachaberger@gmail.com>
2012-01-01 23:52:37 +0100

    LICENSE, EQ, MAP, IFELSE, examples/00-index.html

--- README.md
+++ README.md
@@ -1,42 +1,47 @@
 #avaJScript―think sync, run asyc
 
-This project claims to fight deep nesting of callbacks in ambitious javascript projects involving excessive asynchronous methods.
+This project claims to fight deep nesting of callbacks in ambitious javascript projects involving excessive asynchronous methods. 
 
-##Motivation
+#Motivation
 
 Assuming you wonder where the headache with asynchronous stuff in javascript lies, let's consider the following piece of synchronous code:
 
-	var partyPooper = backend.userForName("Tadaeus");
-	var lastPost = backend.getLastPost(partyPooper);
-	if(lastPost.match("not amused")) {
-		backend.delete(partyPooper);
+	var partyPooper = sync_userForName("Tadaeus");
+	var lastPost = sync_getLastPost(partyPooper);
+	if(sync_isBoring(lastPost)) {
+		sync_delete(partyPooper);
 	}
 	alert("Done.");
 
 If we try to write something simiar assuming that all calls to the backend have to be asynchronous, we come up with something like this:
 
-	backend.userForName("Tadaeus", function(partyPooper){
-		backend.getLastPost(partyPooper, function(lastPost){
-			if(lastPost.match("not amused"))
-				backend.delete(partyPooper, function(){
+	async_userForName("Tadaeus", function(partyPooper){
+		async_getLastPost(partyPooper, function(lastPost){
+			async_isBoring(lastPost, function(isBoring) {
+				if(isBoring)
+					async_delete(partyPooper, function(){
+						alert("Done.");
+					});
+				else {
 					alert("Done.");
-				});
-			else
-				alert("Done.");
+				}
+			})
 		})
 	});
 
-From an architectural point of view there is few to complain about the asynchronous variation (maybe except of the code reduncancy for the alert box). However, the sportive aspects of opening and closing braces and nesting anonymous functions can get demanding. Even more fun arises, when you try to get an asynchronous counterpart of the following synchronous code:
+From an architectural point of view there is few to complain about the asynchronous variation (maybe except of the code reduncancy for the alert box). However, the sportive aspects of opening and closing braces and nesting anonymous functions can get demanding. For me, personally, the most amusing part of the transcripted code is the part after the second alert call, literally "}})})});" (modulo some white spaces and new lines).
 
-	while(backend.userCount() > 0) {
-		backend.deleteRandomUser();
+Even more fun arises, when you try to get an asynchronous counterpart of the following synchronous code:
+
+	while(sync_userCount() > 0) {
+		sync_deleteRandomUser();
 	}
 
-While scratching your forehead, you may realize, that the nested nature of the asynchronous callback mechanism does not fit very well to while-loops, so the first step in asynchronizing the loop may be a recursive approach:
+While scratching your forehead, you may realize, that the nested nature of the asynchronous callback mechanism does not fit very well to while-loops. This is due to the fact, that sequencing asynchronous calls is inevitabely bound to nested callback orchestration. The while construct however, is not aware of callback nesting, as this is not javascripts inherent evaluation model. So, the first step in asynchronizing the loop is to get rid of while, which can be accomplished using recursion:
 
 	var recLoop = function() {
-		if(backend.userCount() > 0) {
-			backend.deleteRandomUser();
+		if(sync_userCount() > 0) {
+			sync_deleteRandomUser();
 			recLoop();
 		}
 	}
@@ -44,13 +49,40 @@ While scratching your forehead, you may realize, that the nested nature of the a
 The asynchronous flavour of recLoop would look like this:
 
 	var asyncRecLoop = function() {
-		backend.userCount(function(count){
+		async_userCount(function(count){
 			if(count > 0) {
-				backend.deleteRandomUser(function(){
+				async_deleteRandomUser(function(){
 					asyncRecLoop();
 				})
 			}
 		})
 	}
 
-For me, personally the most amusing part of the transcripted while loop is the part after the call of asyncRecLoop, literally "})}})}" (modulo some white spaces and new lines).
+So, after this little excursion in anonymous callback kung-fu, how will avaJScript helt you? The goal is, to more or less write code the way you would write it synchronously. As this would involve manipulation of the evaluation behaviour of javascript (breaking with the current ECMA-Script specification), the idea is to provide a domain specific language implemented in javascript, that is able to at least structurally (if not syntactically) reflect the synchronous programs. A avaJScript program for the first synchronous exampe in this section would look as follows:
+
+	LET("partyPooper", as_userForName("Tadaeus")),
+	LET("lastPost", as_getLastPost(GET("partyPooper"))),
+	IF(as_isBoring(GET("lastPost")),
+		as_delete(GET("partyPooper"))
+	),
+	RUN(function(){ 
+		alert("Done."); 
+	})
+
+Yet, all these upper case LETs, GETs, IF and RUN things look very different to javascript, they are indeed all valid javascript function calls, generating an abstract representation of the program that will be evaluated asynchronously, i.e. all the as_... procedures are evaluated asynchronously, such that program evaluation will continue when their results are available.
+
+#Getting Started
+TBD.
+
+#Syntax of avaJScript
+TBD.
+
+#Tutorials
+TBD.
+
+
+#Behind the Scene
+TBD.
+
+#License
+TBD.
---
+++ avajscript.js
@@ -0,0 +1,226 @@
+/*
+Copyright 2011 Sacha Berger
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+(function(){
+	function TRACE/**/(/*varargs*/) {
+		//w/o trace sym. the code has "TR4CE("
+		//replaced by "//TR4CE("
+		console.log.apply(null,arguments);
+	}
+
+	function Scope(outerScope,symbols,result) {
+		this.outerScope=outerScope;
+		this.symbols={};
+		if(symbols!==undefined)
+			for(var k in symbols)
+				this.symbols[k]=symbols[k];
+		if(result!==undefined) this._=result;
+		else this._=undefined;
+		this.setResult = function(value) {
+			//TRACE("set result",value,"in",this);
+			this._=value;
+		};
+		this.getResult = function() {
+			//TRACE("get result",this._,"in",this);
+			return this._;
+		};
+		this.set = function(name,value) {
+			//TRACE("set",name,"to",value,"in",this);
+			if(this.symbols[name] !== undefined) {
+				console.error(name,"already set on",this);
+				throw name+" already set on "+this
+			}
+			this.symbols[name]=value;
+		};
+		this.get = function(name) {
+			//TRACE("get ",name,"in",this);
+			if(name in this.symbols) return this.symbols[name];
+			else if(this.outerScope){
+				try {
+					return this.outerScope.get(name);
+				} catch(e) {
+					console.error(e,this);	
+					throw e
+				}
+			}
+			else {
+				throw "Variable "+name+" not declared.";
+			}
+		};
+	}
+
+	function VAL(primitiveValue) {
+		return function(scope,continuation) {
+			//TRACE("VAL",primitiveValue);
+			scope.setResult(primitiveValue);
+			continuation[0](scope,continuation.slice(1));
+		}
+	}
+
+	function FUN(/*varargs,closure*/){
+		var args = Array.apply(null,arguments);
+		var splt = args.length-1;
+		var closure = args[splt];
+		var formalParams = args.slice(0,splt);
+		// //TRACE("ARGS",formalParams,closure);
+		return function(/*varargs*/) {
+				var args = Array.apply(null,arguments);
+				var toDos = formalParams.map(function(p,i,fps){
+					return LET(p,args[i])
+				});
+			return DO.apply(null,toDos.concat(closure));
+		}
+	}
+
+	function MAP(array,closure) {
+		return DO(
+			array,
+			SYNC(function(scope,chain){
+				var res = scope.getResult().map(function(o){
+					var branchScope = new Scope(scope.outerScope,scope.symbols,o);
+					return branchScope;
+				});
+				var cnt = res.length;
+				res.forEach(function(branchScope,i){
+					//TRACE("MAP:branchScope",branchScope,branchScope.getResult());
+					closure(branchScope,[function(w,_){
+						res[i]=w;
+						cnt--;
+						if(cnt==0) {
+							scope.setResult(res.map(function(o){
+								return o.getResult();
+							}));
+							chain();									
+						}
+					}]);							
+				});
+			})
+		)
+	}
+
+	function IFELSE(condition,Then,Else){
+		return _BLK([
+			condition,
+			function(scope,continuation){
+				var cndVal = scope.getResult();
+				//TRACE("IFELSE:conditionvalue",cndVal);
+				if(cndVal) Then(scope,continuation);
+				else Else(scope,continuation);
+			}
+		])
+	}
+
+	function _BLK(commands){
+		// //TRACE("DEF _BLK",commands);
+		if(commands.length == 0) {
+			return RUN(function(_){});
+		} else {
+			return function(scope,continuation) {
+				// //TRACE("_BLK",scope,continuation);
+				var tailBlk = _BLK(commands.slice(1));
+				commands[0](scope,[tailBlk].concat(continuation));
+			}
+		}
+	}
+
+	function DO(/*varargs*/){
+		var args = Array.apply(null,arguments);
+		return function(scope,continuation) {
+			_BLK(args)(new Scope(scope),[function(innerScope,_) {
+				scope.setResult(innerScope.getResult());
+				continuation[0](scope,continuation.slice(1));
+			}]);
+		}
+	}
+
+	function GET(variable) {
+		return RUN(function(scope) {
+			return scope.get(variable);
+		})
+	}
+
+	function LET(lhs,rhs) {
+		return _BLK([
+			rhs,
+			RUN(function(scope){
+				var value = scope.getResult();
+				//TRACE("LET",lhs,"TO",value);
+				scope.set(lhs,value);					
+			})
+		])
+	}
+
+	function RUN(procedure) {
+		return SYNC(function(scope,chain){
+			// //TRACE("RUN",procedure);
+			var res = procedure(scope);
+			if(res !== undefined)
+				scope.setResult(res);
+			chain();
+		});
+	}
+
+	function SYNC(asyncProcedure) {
+		return function(scope,continuation) {
+			// //TRACE("SYNC",asyncProcedure);
+			asyncProcedure(scope,function(){
+				continuation[0](scope,continuation.slice(1));
+			});
+		}
+	}
+
+	function SCRIPT(/*varargs*/) {
+		this.script = Array.apply(null,arguments);
+		this.callback=function(scope){};
+		this.then = function(callback) {
+			var res = new SCRIPT();
+			res.script=this.script;
+			res.callback=callback;
+			return res;
+		}
+		this.execute = function() {
+			_BLK(this.script)(new Scope(),[this.callback]);	
+		}
+	}
+
+
+	if(! window.sachaberger)
+		window.sachaberger={};
+	
+	if(! window.sachaberger.avaJScript){
+		window.sachaberger.avaJScript={
+			__version__:"pre_1.0_master-branch",
+			VAL: VAL,
+			FUN: FUN,
+			MAP: MAP,
+			DO: DO,
+			GET: GET,
+			LET: LET,
+			RUN: RUN,
+			SYNC: SYNC,
+			SCRIPT: SCRIPT,
+			IFELSE: IFELSE,
+			EQ: FUN("lhs","rhs",RUN(function(scope){
+				var lhs = scope.get("lhs"),
+				    rhs = scope.get("rhs");
+				//TRACE("EQ",lhs,rhs,lhs === rhs);
+				return lhs === rhs
+			}))
+		};
+	}
+})();
+
+
+
